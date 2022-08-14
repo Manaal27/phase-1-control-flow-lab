3bb23a0b4d5de39461494f53ff4b1178f0d49f98
manaal <manaal.basabra@student.moringaschool.com>
2022-08-14 21:06:42 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
-}
+    if (feet<=400) {
+      return 'This one is on me!';
+    } 
+    if (feet >2500) {
+      return 'No can do.';
+    } else if (feet >=2000) {
+      return 'I will gladly take your thirty bucks.';
+    }
+    }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
+function ternaryCheckCity(city){
   // Write your code here!
+ return city === 'NYC'?'Ok, sounds good.' : 'No go.';
+
 }
+    
+   
+   
+   
+
+
+function switchOnCharmFromTip(tip){
+  // Write your code here!
+
+  switch(tip){
+    case 'generous':
+      return 'Thank you so much.'
+    case 'not as generous':
+      return 'Thank you.'
+    default:
+      return 'Bye.'
+  }
+
+} 
